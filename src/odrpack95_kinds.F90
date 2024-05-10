406b074cf3e716abc6e7ca06968377d6a6c5b63f
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2024-05-11 00:20:06 +0200

    add kinds module

---
+++ src/odrpack95_kinds.F90
@@ -0,0 +1,20 @@
+module odrpack95_kinds
+    !! Real kinds and common numeric constants.
+    use, intrinsic :: iso_fortran_env, only: real32, real64
+    implicit none
+    private
+
+    public :: wp
+    public :: ZERO, ONE, NEGONE
+    
+#ifdef REAL32
+    integer, parameter :: wp = real32
+#elif REAL64
+    integer, parameter :: wp = real64
+#else
+    integer, parameter :: wp = real64
+#endif
+    
+    real(wp), parameter :: ZERO = 0.0_wp, ONE = 1.0_wp, NEGONE = -1.0_wp
+    
+end module odrpack95_kinds
