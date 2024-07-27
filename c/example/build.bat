3484f1400bd03b21357bf56691415fee94338a26
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2024-07-27 20:44:06 +0200

    update build scripts

--- c/example/build.bat
+++ c/example/build.bat
@@ -9,13 +9,16 @@ if not exist %BUILD_DIR% (
 )
 
 rem Compile Fortran files to object files
-gfortran %COMPILE_FLAGS% -c %SRC_DIR%\odrpack_kinds.F90 -o %BUILD_DIR%\odrpack_kinds.o
-gfortran %COMPILE_FLAGS% -c %SRC_DIR%\lpkbls.f          -o %BUILD_DIR%\lpkbls.o
-gfortran %COMPILE_FLAGS% -c %SRC_DIR%\odrpack.f90       -o %BUILD_DIR%\odrpack.o
-gfortran %COMPILE_FLAGS% -c ..\odrpack_c.f90            -o %BUILD_DIR%\odrpack_c.o
+gfortran %COMPILE_FLAGS% -c %SRC_DIR%\odrpack_kinds.F90   -o %BUILD_DIR%\odrpack_kinds.o
+gfortran %COMPILE_FLAGS% -c %SRC_DIR%\blas.f              -o %BUILD_DIR%\blas.o
+gfortran %COMPILE_FLAGS% -c %SRC_DIR%\linpack.f           -o %BUILD_DIR%\linpack.o
+gfortran %COMPILE_FLAGS% -c %SRC_DIR%\odrpack_core.f90    -o %BUILD_DIR%\odrpack_core.o
+gfortran %COMPILE_FLAGS% -c %SRC_DIR%\odrpack_reports.f90 -o %BUILD_DIR%\odrpack_reports.o
+gfortran %COMPILE_FLAGS% -c %SRC_DIR%\odrpack.f90         -o %BUILD_DIR%\odrpack.o
+gfortran %COMPILE_FLAGS% -c %SRC_DIR%\odrpack_capi.f90    -o %BUILD_DIR%\odrpack_capi.o
 
 rem Create the static library
-ar rcs %BUILD_DIR%\libodrpack.a %BUILD_DIR%\odrpack_kinds.o %BUILD_DIR%\lpkbls.o %BUILD_DIR%\odrpack.o %BUILD_DIR%\odrpack_c.o
+ar rcs %BUILD_DIR%\libodrpack.a %BUILD_DIR%\odrpack_kinds.o %BUILD_DIR%\blas.o %BUILD_DIR%\linpack.o %BUILD_DIR%\odrpack_core.o %BUILD_DIR%\odrpack_reports.o %BUILD_DIR%\odrpack.o %BUILD_DIR%\odrpack_capi.o
 
 rem Compile C example
 gcc %COMPILE_FLAGS% example5.c -L%BUILD_DIR% -lodrpack -lgfortran -o %BUILD_DIR%\example5.exe
--- c/example/build.sh
+++ c/example/build.sh
@@ -7,17 +7,20 @@ SRC_DIR="../../src"
 
 # Create build directory if it does not exist
 if [ ! -d "$BUILD_DIR" ]; then
-    mkdir -p "$BUILD_DIR"
+    mkdir "$BUILD_DIR"
 fi
 
 # Compile Fortran files to object files
-gfortran $COMPILE_FLAGS -c $SRC_DIR/odrpack_kinds.F90 -o $BUILD_DIR/odrpack_kinds.o
-gfortran $COMPILE_FLAGS -c $SRC_DIR/lpkbls.f          -o $BUILD_DIR/lpkbls.o
-gfortran $COMPILE_FLAGS -c $SRC_DIR/odrpack.f90       -o $BUILD_DIR/odrpack.o
-gfortran $COMPILE_FLAGS -c ../odrpack_c.f90           -o $BUILD_DIR/odrpack_c.o
+gfortran $COMPILE_FLAGS -c "$SRC_DIR/odrpack_kinds.F90"   -o "$BUILD_DIR/odrpack_kinds.o"
+gfortran $COMPILE_FLAGS -c "$SRC_DIR/blas.f"              -o "$BUILD_DIR/blas.o"
+gfortran $COMPILE_FLAGS -c "$SRC_DIR/linpack.f"           -o "$BUILD_DIR/linpack.o"
+gfortran $COMPILE_FLAGS -c "$SRC_DIR/odrpack_core.f90"    -o "$BUILD_DIR/odrpack_core.o"
+gfortran $COMPILE_FLAGS -c "$SRC_DIR/odrpack_reports.f90" -o "$BUILD_DIR/odrpack_reports.o"
+gfortran $COMPILE_FLAGS -c "$SRC_DIR/odrpack.f90"         -o "$BUILD_DIR/odrpack.o"
+gfortran $COMPILE_FLAGS -c "$SRC_DIR/odrpack_capi.f90"    -o "$BUILD_DIR/odrpack_capi.o"
 
 # Create the static library
-ar rcs $BUILD_DIR/libodrpack.a $BUILD_DIR/odrpack_kinds.o $BUILD_DIR/lpkbls.o $BUILD_DIR/odrpack.o $BUILD_DIR/odrpack_c.o
+ar rcs "$BUILD_DIR/libodrpack.a" "$BUILD_DIR/odrpack_kinds.o" "$BUILD_DIR/blas.o" "$BUILD_DIR/linpack.o" "$BUILD_DIR/odrpack_core.o" "$BUILD_DIR/odrpack_reports.o" "$BUILD_DIR/odrpack.o" "$BUILD_DIR/odrpack_capi.o"
 
 # Compile C example
-gcc $COMPILE_FLAGS example5.c -L$BUILD_DIR -lodrpack -lgfortran -o $BUILD_DIR/example5
+gcc $COMPILE_FLAGS example5.c -L"$BUILD_DIR" -lodrpack -lgfortran -o "$BUILD_DIR/example5.exe"
