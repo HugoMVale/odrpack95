fe565e2384ad92346d7f1a28ed538c9b90408ca5
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2024-08-03 17:31:51 +0200

    comment out sp interfaces

--- src/blas_interfaces.f90
+++ src/blas_interfaces.f90
@@ -5,13 +5,13 @@ module blas_interfaces
 
    interface
 
-      pure real(sp) function sasum(n, x, incx)
-      !! Sum of magnitudes of vector components.
-         import :: sp
-         integer, intent(in) :: n
-         real(sp), intent(in) :: x(*)
-         integer, intent(in) :: incx
-      end function sasum
+      ! pure real(sp) function sasum(n, x, incx)
+      ! !! Sum of magnitudes of vector components.
+      !    import :: sp
+      !    integer, intent(in) :: n
+      !    real(sp), intent(in) :: x(*)
+      !    integer, intent(in) :: incx
+      ! end function sasum
 
       pure real(dp) function dasum(n, x, incx)
       !! Sum of magnitudes of vector components.
@@ -21,16 +21,16 @@ module blas_interfaces
          integer, intent(in) :: incx
       end function dasum
 
-      pure subroutine saxpy(n, a, x, incx, y, incy)
-      !! Computation `Y = A*X + Y`.
-         import :: sp
-         integer, intent(in) :: n
-         real(sp), intent(in) :: a
-         real(sp), intent(in) :: x(*)
-         integer, intent(in) :: incx
-         real(sp), intent(inout) :: y(*)
-         integer, intent(in) :: incy
-      end subroutine saxpy
+      ! pure subroutine saxpy(n, a, x, incx, y, incy)
+      ! !! Computation `Y = A*X + Y`.
+      !    import :: sp
+      !    integer, intent(in) :: n
+      !    real(sp), intent(in) :: a
+      !    real(sp), intent(in) :: x(*)
+      !    integer, intent(in) :: incx
+      !    real(sp), intent(inout) :: y(*)
+      !    integer, intent(in) :: incy
+      ! end subroutine saxpy
 
       pure subroutine daxpy(n, a, x, incx, y, incy)
       !! Computation `Y = A*X + Y`.
@@ -43,15 +43,15 @@ module blas_interfaces
          integer, intent(in) :: incy
       end subroutine daxpy
 
-      pure subroutine scopy(n, x, incx, y, incy)
-      !! Vector copy `Y = X`.
-         import :: sp
-         integer, intent(in) :: n
-         real(sp), intent(in) :: x(*)
-         integer, intent(in) :: incx
-         real(sp), intent(inout) :: y(*)
-         integer, intent(in) :: incy
-      end subroutine scopy
+      ! pure subroutine scopy(n, x, incx, y, incy)
+      ! !! Vector copy `Y = X`.
+      !    import :: sp
+      !    integer, intent(in) :: n
+      !    real(sp), intent(in) :: x(*)
+      !    integer, intent(in) :: incx
+      !    real(sp), intent(inout) :: y(*)
+      !    integer, intent(in) :: incy
+      ! end subroutine scopy
 
       pure subroutine dcopy(n, x, incx, y, incy)
       !! Vector copy `Y = X`.
@@ -63,15 +63,15 @@ module blas_interfaces
          integer, intent(in) :: incy
       end subroutine dcopy
 
-      pure real(sp) function sdot(n, x, incx, y, incy)
-      !! Inner product of vectors.
-         import :: sp
-         integer, intent(in) :: n
-         real(sp), intent(in) :: x(*)
-         integer, intent(in) :: incx
-         real(sp), intent(in) :: y(*)
-         integer, intent(in) :: incy
-      end function sdot
+      ! pure real(sp) function sdot(n, x, incx, y, incy)
+      ! !! Inner product of vectors.
+      !    import :: sp
+      !    integer, intent(in) :: n
+      !    real(sp), intent(in) :: x(*)
+      !    integer, intent(in) :: incx
+      !    real(sp), intent(in) :: y(*)
+      !    integer, intent(in) :: incy
+      ! end function sdot
 
       pure real(dp) function ddot(n, x, incx, y, incy)
       !! Inner product of vectors.
@@ -83,13 +83,13 @@ module blas_interfaces
          integer, intent(in) :: incy
       end function ddot
 
-      pure real(sp) function snrm2(n, x, incx)
-      !! Euclidean length (L2 Norm) of vector.
-         import :: sp
-         integer, intent(in) :: n
-         real(sp), intent(in) :: x(*)
-         integer, intent(in) :: incx
-      end function snrm2
+      ! pure real(sp) function snrm2(n, x, incx)
+      ! !! Euclidean length (L2 Norm) of vector.
+      !    import :: sp
+      !    integer, intent(in) :: n
+      !    real(sp), intent(in) :: x(*)
+      !    integer, intent(in) :: incx
+      ! end function snrm2
 
       pure real(dp) function dnrm2(n, x, incx)
       !! Euclidean length (L2 Norm) of vector.
@@ -99,17 +99,17 @@ module blas_interfaces
          integer, intent(in) :: incx
       end function dnrm2
 
-      pure subroutine srot(n, x, incx, y, incy, c, s)
-      !! Apply Givens rotation.
-         import :: sp
-         integer, intent(in) :: n
-         real(sp), intent(inout) :: x(*)
-         integer, intent(in) :: incx
-         real(sp), intent(inout) :: y(*)
-         integer, intent(in) :: incy
-         real(sp), intent(in) :: c
-         real(sp), intent(in) :: s
-      end subroutine srot
+      ! pure subroutine srot(n, x, incx, y, incy, c, s)
+      ! !! Apply Givens rotation.
+      !    import :: sp
+      !    integer, intent(in) :: n
+      !    real(sp), intent(inout) :: x(*)
+      !    integer, intent(in) :: incx
+      !    real(sp), intent(inout) :: y(*)
+      !    integer, intent(in) :: incy
+      !    real(sp), intent(in) :: c
+      !    real(sp), intent(in) :: s
+      ! end subroutine srot
 
       pure subroutine drot(n, x, incx, y, incy, c, s)
       !! Apply Givens rotation.
@@ -124,14 +124,14 @@ module blas_interfaces
          real(dp), intent(in) :: s
       end subroutine drot
 
-      pure subroutine srotg(a, b, c, s)
-      !! Construct plane Givens rotation.
-         import :: sp
-         real(sp), intent(in) :: a
-         real(sp), intent(in) :: b
-         real(sp), intent(out) :: c
-         real(sp), intent(out) :: s
-      end subroutine srotg
+      ! pure subroutine srotg(a, b, c, s)
+      ! !! Construct plane Givens rotation.
+      !    import :: sp
+      !    real(sp), intent(in) :: a
+      !    real(sp), intent(in) :: b
+      !    real(sp), intent(out) :: c
+      !    real(sp), intent(out) :: s
+      ! end subroutine srotg
 
       pure subroutine drotg(a, b, c, s)
       !! Construct plane Givens rotation.
@@ -142,14 +142,14 @@ module blas_interfaces
          real(dp), intent(out) :: s
       end subroutine drotg
 
-      pure subroutine sscal(n, a, x, incx)
-      !! Vector scale `X = A*X`.
-         import :: sp
-         integer, intent(in) :: n
-         real(sp), intent(in) :: a
-         real(sp), intent(inout) :: x(*)
-         integer, intent(in) :: incx
-      end subroutine sscal
+      ! pure subroutine sscal(n, a, x, incx)
+      ! !! Vector scale `X = A*X`.
+      !    import :: sp
+      !    integer, intent(in) :: n
+      !    real(sp), intent(in) :: a
+      !    real(sp), intent(inout) :: x(*)
+      !    integer, intent(in) :: incx
+      ! end subroutine sscal
 
       pure subroutine dscal(n, a, x, incx)
       !! Vector scale `X = A*X`.
@@ -160,15 +160,15 @@ module blas_interfaces
          integer, intent(in) :: incx
       end subroutine dscal
 
-      pure subroutine sswap(n, x, incx, y, incy)
-      !! Interchange vectors.
-         import :: sp
-         integer, intent(in) :: n
-         real(sp), intent(in) :: x(*)
-         integer, intent(in) :: incx
-         real(sp), intent(inout) :: y(*)
-         integer, intent(in) :: incy
-      end subroutine sswap
+      ! pure subroutine sswap(n, x, incx, y, incy)
+      ! !! Interchange vectors.
+      !    import :: sp
+      !    integer, intent(in) :: n
+      !    real(sp), intent(in) :: x(*)
+      !    integer, intent(in) :: incx
+      !    real(sp), intent(inout) :: y(*)
+      !    integer, intent(in) :: incy
+      ! end subroutine sswap
 
       pure subroutine dswap(n, x, incx, y, incy)
       !! Interchange vectors.
@@ -180,13 +180,13 @@ module blas_interfaces
          integer, intent(in) :: incy
       end subroutine dswap
 
-      pure integer function isamax(n, x, incx)
-      !! Find largest component of vector.
-         import :: sp
-         integer, intent(in) :: n
-         real(sp), intent(in) :: x(*)
-         integer, intent(in) :: incx
-      end function isamax
+      ! pure integer function isamax(n, x, incx)
+      ! !! Find largest component of vector.
+      !    import :: sp
+      !    integer, intent(in) :: n
+      !    real(sp), intent(in) :: x(*)
+      !    integer, intent(in) :: incx
+      ! end function isamax
 
       pure integer function idamax(n, x, incx)
       !! Find largest component of vector.
