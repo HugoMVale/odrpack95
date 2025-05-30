9e35fd53071a3790dfc523f45fd9cf0d28cd3484
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2025-05-30 22:07:18 +0200

    check fscanf  result

--- c/example/example3.c
+++ c/example/example3.c
@@ -2,9 +2,10 @@
 This is a translation of example 3 from the ODRPACK95 documentation.
 */
 
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+
 #include "../include/odrpack/odrpack.h"
 
 // User-supplied function for evaluating the model
@@ -12,18 +13,14 @@ void fcn(const int *n, const int *m, const int *np, const int *nq,
          const int *ldn, const int *ldm, const int *ldnp,
          const double beta[], const double xplusd[],
          const int ifixb[], const int ifixx[], const int *ldifx, const int *ideval,
-         double f[], double fjacb[], double fjacd[], int *istop)
-{
-
+         double f[], double fjacb[], double fjacd[], int *istop) {
     // Local variables
     double freq, omega, ctheta, stheta, theta, phi, r;
     const double pi = 4 * atan(1.0);
 
     // Check for unacceptable values for this problem
-    for (int i = 0; i < *n; i++)
-    {
-        if (xplusd[i * *m] < 0.0)
-        {
+    for (int i = 0; i < *n; i++) {
+        if (xplusd[i * *m] < 0.0) {
             *istop = 1;
             return;
         }
@@ -35,10 +32,8 @@ void fcn(const int *n, const int *m, const int *np, const int *nq,
     stheta = sin(theta);
 
     // Model function
-    if ((*ideval % 10) >= 1)
-    {
-        for (int i = 0; i < *n; i++)
-        {
+    if ((*ideval % 10) >= 1) {
+        for (int i = 0; i < *n; i++) {
             freq = xplusd[i];
             omega = pow(2.0 * pi * freq * exp(-beta[2]), beta[3]);
             phi = atan2(omega * stheta, 1 + omega * ctheta);
@@ -49,9 +44,7 @@ void fcn(const int *n, const int *m, const int *np, const int *nq,
     }
 }
 
-int main()
-{
-
+int main() {
 #define NP 5
 #define N 23
 #define M 1
@@ -63,73 +56,70 @@ int main()
     double beta[NP], x[M][N], y[NQ][N], delta[M][N], wd[M][M][N], we[NQ][NQ][N];
     int ifixx[M][N];
     int ifixb[NP] = {1, 1, 1, 1, 1};
+    int iscan = 0;
 
     // Read problem data
     FILE *data_file = fopen("./data3.dat", "r");
-    for (int i = 0; i < NP; i++)
-    {
-        fscanf(data_file, "%lf", &beta[i]);
-    }
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            fscanf(data_file, "%lf", &x[j][i]);
+
+    for (int i = 0; i < NP; i++) {
+        iscan = fscanf(data_file, "%lf", &beta[i]);
+        if (iscan != 1) {
+            fprintf(stderr, "Error reading beta values\n");
+            fclose(data_file);
+            return 1;
         }
-        for (int j = 0; j < nq; j++)
-        {
-            fscanf(data_file, "%lf", &y[j][i]);
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            iscan = fscanf(data_file, "%lf", &x[j][i]);
+            if (iscan != 1) {
+                fprintf(stderr, "Error reading x values\n");
+                fclose(data_file);
+                return 1;
+            }
+        }
+        for (int j = 0; j < nq; j++) {
+            iscan = fscanf(data_file, "%lf", &y[j][i]);
+            if (iscan != 1) {
+                fprintf(stderr, "Error reading y values\n");
+                fclose(data_file);
+                return 1;
+            }
         }
     }
     fclose(data_file);
 
     // Initialize delta and specify first decade of frequencies as fixed
-    for (int i = 0; i < n; i++)
-    {
-        if (x[0][i] < 100.0)
-        {
+    for (int i = 0; i < n; i++) {
+        if (x[0][i] < 100.0) {
             delta[0][i] = 0.0;
             ifixx[0][i] = 0;
-        }
-        else if (x[0][i] <= 150.0)
-        {
+        } else if (x[0][i] <= 150.0) {
             delta[0][i] = 0.0;
             ifixx[0][i] = 1;
-        }
-        else if (x[0][i] <= 1000.0)
-        {
+        } else if (x[0][i] <= 1000.0) {
             delta[0][i] = 25.0;
             ifixx[0][i] = 1;
-        }
-        else if (x[0][i] <= 10000.0)
-        {
+        } else if (x[0][i] <= 10000.0) {
             delta[0][i] = 560.0;
             ifixx[0][i] = 1;
-        }
-        else if (x[0][i] <= 100000.0)
-        {
+        } else if (x[0][i] <= 100000.0) {
             delta[0][i] = 9500.0;
             ifixx[0][i] = 1;
-        }
-        else
-        {
+        } else {
             delta[0][i] = 144000.0;
             ifixx[0][i] = 1;
         }
     }
 
     // Set weights
-    for (int i = 0; i < n; i++)
-    {
-        if (x[0][i] == 100.0 || x[0][i] == 150.0)
-        {
+    for (int i = 0; i < n; i++) {
+        if (x[0][i] == 100.0 || x[0][i] == 150.0) {
             we[0][0][i] = 0.0;
             we[1][0][i] = 0.0;
             we[0][1][i] = 0.0;
             we[1][1][i] = 0.0;
-        }
-        else
-        {
+        } else {
             we[0][0][i] = 559.6;
             we[1][0][i] = -1634.0;
             we[0][1][i] = -1634.0;
@@ -142,8 +132,8 @@ int main()
     int job = 1010;
     int iprint = 2002;
 
-    int lunrpt = 6; // stdout
-    int lunerr = 6; // stderr
+    int lunrpt = 6;  // stdout
+    int lunerr = 6;  // stderr
     int info = 0;
 
     // Compute solution
